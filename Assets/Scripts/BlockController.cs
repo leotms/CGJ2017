0c347eb3210946fd8951387bb06af67a2ffc66d7
Edward <edwardfernande26@gmail.com>
2017-01-21 16:00:04 -0400

    La onda expansiva aparece, falta la estetica.

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+	float time = 0.05f;
+	bool canShow = true;
+	bool stopCoroutine;
+	bool startedCoroutine;
+	IEnumerator coroutine;
+	GameObject children;
+
+	// Use this for initialization
+	void Start () {
+		coroutine = ActiveBlock ();
+		startedCoroutine = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameObject.GetComponent<SpriteRenderer>().enabled && !startedCoroutine && canShow) {
+			StartCoroutine (coroutine);
+			stopCoroutine = false;
+			startedCoroutine = true;
+		}
+
+		// When the timer finish I need to set active the item.
+		if (stopCoroutine) {
+			StopCoroutine (coroutine);
+
+			gameObject.GetComponent<BoxCollider2D> ().enabled = true;
+
+			if (canShow) {
+				gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+			}
+			startedCoroutine = false;
+		}
+	}
+
+	// This let you know when the pickut will activate.
+	IEnumerator ActiveBlock(){
+		while (true) {
+			yield return new WaitForSeconds (time);
+			stopCoroutine = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Tinker"){
+			canShow = false;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		canShow = true;
+	}
+}
---
+++ Assets/tinkerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tinkerController : MonoBehaviour {
+
+	bool activate;
+	bool expanded;
+	bool reduce;
+	float minRadius;
+	float maxRadius;
+
+	CircleCollider2D cCollider;
+
+	float time = 5f;
+	bool stopCoroutine;
+	bool startedCoroutine;
+	IEnumerator coroutine;
+
+	void Start(){
+		minRadius = 0.55f;
+		maxRadius = 2f;
+		activate = false;
+		expanded = false;
+		reduce = false;
+		cCollider = gameObject.GetComponent<CircleCollider2D> ();
+
+		coroutine = HideTinker ();
+		startedCoroutine = false;
+	}
+
+	void Update(){
+
+		if (Input.GetKey (KeyCode.C)) {
+			activate = true;
+		}
+
+		if (activate && !expanded) {
+			if (cCollider.radius < maxRadius)
+				cCollider.radius += 0.01f;
+			else {
+				expanded = true;
+				reduce = true;
+			}
+		}
+
+		if (expanded && reduce) {
+			if (cCollider.radius > minRadius) {
+				print ("reduce");
+				cCollider.radius -= 0.05f;
+			} else {
+				reduce = false;
+				activate = false;
+				expanded = false;
+			}
+		}
+
+		if (expanded && !startedCoroutine) {
+			StartCoroutine (coroutine);
+			stopCoroutine = false;
+			startedCoroutine = true;
+		}
+
+		// When the timer finish I need to set active the item.
+		if (stopCoroutine) {
+			StopCoroutine (coroutine);
+
+			reduce = true;
+			startedCoroutine = false;
+		}
+	}
+
+
+ 	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Darkness"){
+			other.gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		}
+    }
+
+	IEnumerator HideTinker(){
+		while (true) {
+			yield return new WaitForSeconds (time);
+			stopCoroutine = true;
+		}
+	}
+}
