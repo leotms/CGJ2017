5d2b5e187d702468bfcdbf44ea9be30511cc9ee3
leotms <martinezazuaje@gmail.com>
2017-01-22 08:46:57 -0400

    We still got hope

---
+++ Assets/Scripts/BellController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellController : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Darkness"){
+			other.gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		}
+	}
+		
+}
