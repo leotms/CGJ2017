2bc277225da59e23ea7762374f6cffdad196602f
leotms <martinezazuaje@gmail.com>
2017-01-22 13:43:24 -0400

    Lista pantalla de carga.

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -1,17 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour {
 
 	float timeLeft = 30.0f;
+	public Text timer;
+	bool loadScene = true;
+
+	void Start(){
+		timer.color = new Color(0.72f, 0.7f, 0.14f);
+	}
 
 	void Update() {
 		timeLeft -= Time.deltaTime;
 		if(timeLeft >= 0)
 		{
-			print (timeLeft);
+			UpdateText ();
+		}
+
+		if (timeLeft <= 0 && loadScene) {
+			loadScene = false;
+			StartCoroutine (Loading ());
 		}
 	}
 		
+	void UpdateText(){
+		if (timeLeft >= 10)
+			timer.text = "Sobrevive hasta que se acabe el tiempo: " + timeLeft.ToString ("N0");
+		else {
+			timer.text = "Sobrevive hasta que se acabe el tiempo: 0" + timeLeft.ToString ("N0");
+		}
+	}
+
+	IEnumerator Loading(){
+		yield return new WaitForSeconds (1);
+		AsyncOperation async = Application.LoadLevelAsync (4);
+
+		while (!async.isDone)
+			yield return null;
+	}
+
 }
