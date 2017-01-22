4689f7c42655f5b859a644250f4cdaf692a4f2f1
Edward <edwardfernande26@gmail.com>
2017-01-22 13:41:24 -0400

    Cabios en los niveles

---
+++ Assets/Scripts/TinkerControllerInicio.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TinkerControllerInicio : MonoBehaviour {
+
+	bool activate;
+	bool expanded;
+	bool reduce;
+	float minRadius;
+	float maxRadius;
+
+	CircleCollider2D cCollider;
+
+	float time = 5f;
+	bool stopCoroutine;
+	bool startedCoroutine;
+	IEnumerator coroutine;
+
+	float timeToExpand = 0.6f;
+	bool stopCoroutineExpand;
+	bool startedCoroutineExpand;
+	IEnumerator coroutineExpand;
+
+
+
+	public Transform Tinker_Scale;
+	public GameObject tinker;
+	Animator tinkerAnim;
+
+	public bool canExpand = true;
+
+	void Start(){
+		minRadius = 0.55f;
+		maxRadius = 1.5f;
+		activate = true;
+		expanded = false;
+		reduce = false;
+		cCollider = gameObject.GetComponent<CircleCollider2D> ();
+
+		coroutine = HideTinker ();
+		startedCoroutine = false;
+
+		coroutineExpand = WaitTime ();
+		startedCoroutineExpand = false;
+
+		tinkerAnim = tinker.GetComponent<Animator> ();
+	}
+
+	void Update(){
+
+		if (Input.GetKey (KeyCode.C) && !tinkerAnim.GetCurrentAnimatorStateInfo(0).IsName("reduceTinker") && canExpand) {
+			activate = true;
+			canExpand = false;
+		}
+
+		if (activate && !expanded) {
+			tinkerAnim.SetBool ("expand", true);
+			if (cCollider.radius < maxRadius) {
+				cCollider.radius += 0.025f;
+				Tinker_Scale.localScale += new Vector3(0.17f, 0.17f, 0);
+			}else {
+				expanded = true;
+				reduce = true;
+			}
+		}
+			
+		if (expanded && reduce) {
+			tinkerAnim.SetBool ("reduce", true);
+			if (cCollider.radius > minRadius) {
+				print ("reduce");
+				cCollider.radius -= 0.025f;
+				Tinker_Scale.localScale -= new Vector3 (0.08f, 0.08f, 0);
+			} else {
+				reduce = false;
+				activate = true;
+				expanded = false;
+				tinkerAnim.SetBool ("expand", false);
+				tinkerAnim.SetBool ("reduce", false);
+
+				if (!startedCoroutineExpand) {
+					StartCoroutine (coroutineExpand);
+					stopCoroutineExpand = false;
+					startedCoroutineExpand = true;
+				}
+			}
+		}
+
+		if (expanded && !startedCoroutine) {
+			StartCoroutine (coroutine);
+			stopCoroutine = false;
+			startedCoroutine = true;
+		}
+
+		// When the timer finish I need to set active the item.
+		if (stopCoroutine) {
+			StopCoroutine (coroutine);
+
+			reduce = true;
+			startedCoroutine = false;
+		}
+
+		if (stopCoroutineExpand) {
+			StopCoroutine (coroutineExpand);
+
+			canExpand = true;
+		}
+	}
+
+
+ 	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Darkness"){
+			other.gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		}
+    }
+
+	IEnumerator HideTinker(){
+		while (true) {
+			yield return new WaitForSeconds (time);
+			stopCoroutine = true;
+		}
+	}
+
+	IEnumerator WaitTime(){
+		while (true) {
+			yield return new WaitForSeconds (timeToExpand);
+			stopCoroutineExpand = true;
+		}
+	}
+}
