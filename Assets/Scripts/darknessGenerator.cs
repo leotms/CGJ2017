fcadd11fbaf7b7a23e587815cbe053363b08f957
Edward <edwardfernande26@gmail.com>
2017-01-22 02:04:21 -0400

    Adelantado prototipo niveles.

--- Assets/Scripts/darknessGenerator.cs
+++ Assets/Scripts/darknessGenerator.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class darknessGenerator : MonoBehaviour {
 
 	public GameObject darknessBlock;
+	public float limitx;
+	public float limity;
 
 	void Awake(){
 
-		for(float i = -2.5f; i < 2.5f ; i += 0.05f){
-			for(float j = -1.5f; j < 1.5f ; j += 0.05f){
+		for(float i = -limitx; i < limitx ; i += 0.05f){
+			for(float j = -limity; j < limity ; j += 0.05f){
 				GameObject dInstance = Instantiate(darknessBlock, new Vector3(i, j, -1), Quaternion.identity);
 				dInstance.transform.parent = transform;
 			}
---
+++ Assets/heartController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heartController : MonoBehaviour {
+
+	int life;
+
+	// Use this for initialization
+	void Start () {
+		life = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Enemy") {
+			life -= 1;
+			print (life);
+		}
+
+	}
+}
